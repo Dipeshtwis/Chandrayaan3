f552eec20922dca3e9ae3c539057bb6c1c3c9c6f
Dipeshtwis <deepesh.97kumar@gmail.com>
2024-01-30 21:01:35 +0530

    test to give initial position and direction

---
+++ spec/chandrayaan3_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../chandrayaan3'
+
+describe Chandrayaan3 do
+  describe '#initialize' do
+    it 'initializes with a starting position and direction' do
+      spacecraft = Chandrayaan3.new([0, 0, 0], 'N')
+      expect(spacecraft.position).to eq([0, 0, 0])
+      expect(spacecraft.direction).to eq('N')
+    end
+  end
+end
