4880fd02a18409131bac7fb044b67b8992d8e6db
Dipeshtwis <deepesh.97kumar@gmail.com>
2024-01-31 19:49:55 +0530

    Add the code to execute command forward, right, and up

--- user_input_handler.rb
+++ user_input_handler.rb
@@ -5,5 +5,16 @@ class UserInputHandler
   end
 
   def self.execute_commands(commands, spacecraft)
+    commands.each { |command| execute_command(command, spacecraft) }
+  end
+
+  private
+
+  def self.execute_command(command, spacecraft)
+    case command
+      when 'f' then spacecraft.move_forward
+      when 'r' then spacecraft.turn_right
+      when 'u' then spacecraft.rotate_up
+    end
   end
 end
